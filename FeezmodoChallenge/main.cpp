fd3fa3bad41e1a9d81bbf58964d11b6d155ff9c2
ShreyasKp <shreyaskp1999@gmail.com>
2018-11-19 13:46:30 +0800

    Add ability to update number of people

--- FeezmodoChallenge/main.cpp
+++ FeezmodoChallenge/main.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 map <string, vector <string>> AL;
-const char *arr[] = {"Andy", "Ben", "Carl", "Denise", "Elena", "Fiona"};
+string arr[100];
+int originalCount = 6;
 
 int BFS(string start, string finish) {
     map<string, bool> visited;
@@ -30,8 +31,33 @@ int BFS(string start, string finish) {
     }
     return distance[finish];
 }
+
+void addPerson() {
+    string newPerson, neighbour;
+    int cnt;
+    cin.get();
+    cout << "Enter name of new person: ";
+    getline(cin, newPerson);
+    cout << "Enter the number of connections: ";
+    cin >> cnt;
+    cin.get();
+    for (int i = 0; i < cnt; i++) {
+        cout << "Enter " << (i+1) << "th connection: ";
+        getline(cin, neighbour);
+        AL[newPerson].push_back(neighbour);
+        AL[neighbour].push_back(newPerson);
+        arr[originalCount++] = newPerson;
+    }
+    cout << "Person added" << endl;
+}
 int main()
 {
+    arr[0] = "Andy";
+    arr[1] = "Ben";
+    arr[2] = "Carl";
+    arr[3] = "Denise";
+    arr[4] = "Elena";
+    arr[5] = "Fiona";
     AL["Andy"].push_back("Ben");
     AL["Ben"].push_back("Andy");
     AL["Ben"].push_back("Carl");
@@ -45,16 +71,30 @@ int main()
     AL["Fiona"].push_back("Elena");
     AL["Fiona"].push_back("Denise");
 
-    string name1, name2;
-    cout << "First name: ";
-    getline(cin, name1);
-    cout << "Second name: ";
-    getline(cin, name2);
+    int option;
+    cout << "Press 1 for adding new persons" << endl;
+    cout << "Press 2 for finding degree of separation" << endl;
+    cout << "Enter your choice: ";
+    cin >> option;
+    switch(option) {
+    case 1:
+        addPerson();
+        break;
+    case 2:
+        string name1, name2;
+        cin.get();
+        cout << "First name: ";
+        getline(cin, name1);
 
-    int degree = BFS(name1, name2);
-    if(degree != 100)
-        cout << "Degree of Separation = " << BFS(name1, name2) << endl;
-    else
-        cout << "Incorrect name" << endl;
+        cout << "Second name: ";
+        getline(cin, name2);
+
+        int degree = BFS(name1, name2);
+        if(degree != 100)
+            cout << "Degree of Separation = " << BFS(name1, name2) << endl;
+        else
+            cout << "Incorrect name" << endl;
+        break;
+    }
     return 0;
 }
